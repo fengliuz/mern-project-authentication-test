551fceafe569a6f90fdcfe29c6b6c7d1fc881873
fengliuz <fengliuzng4a@gmail.com>
2026-03-13 23:44:47 +0700

    feat: ProductController

---
+++ backend/src/app/Controllers/ProductController.js
@@ -0,0 +1,47 @@
+import Product from "../Models/Product";
+
+export const createCategory = async (req, res) => {
+  try {
+    const { sku, name, description, price, stock, minStock, unit, categoryId } =
+      req.body;
+    const existingProduct = await Product.findOne({ sku });
+    if (existingProduct) {
+      return res.status(400).json({ message: "SKU Already Exists" });
+    }
+
+    const newProduct = await Product.crate({
+      sku,
+      name,
+      description,
+      price,
+      stock,
+      minStock,
+      unit,
+      category: categoryId,
+      createdBy: req.user._id,
+    });
+    res.status(201).json({
+      message: "A new Product created successfully",
+      data: newCategory,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed create new Product", error: error.message });
+  }
+};
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Category.find().populate("category","name").populate("createdBy","username");
+    res.status(200).json({
+      message: "Success retrieved all data from products",
+      data: { products },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to retieved all data from products ",
+      error: error.message,
+    });
+  }
+};
+s
