334effbeb7967f87f493795273763b1a2cdb9540
fengliuz <fengliuzng4a@gmail.com>
2026-03-20 01:20:52 +0700

    fix8: deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -71,7 +71,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../FRONTEND", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 if (process.env.NODE_ENV !== "production") {
